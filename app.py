d6529d0b162801e9f7967c50683322b3054d5e12
Isabelle Fernandes <isabellefeju@gmail.com>
2026-05-16 14:51:53 -0300

    estrutura inicial frontend

---
+++ app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="TechKitfy",
+    page_icon="🖥️",
+    layout="wide"
+)
+
+st.title("🖥️ TechKitfy")
+st.subheader("Sistema de Loja de Tecnologia")
+
+st.write("Projeto APS - Frontend")
